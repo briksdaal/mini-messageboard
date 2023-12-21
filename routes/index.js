0d570b1d8c6a92ace6131e6e98b0884da1bedd5e
briksdaal <102250834+briksdaal@users.noreply.github.com>
2023-12-21 17:22:48 +0200

    Add homepage and new message page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Boris',
+    added: new Date(),
+  },
+  {
+    text: 'We are!',
+    user: 'Luffy',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  req.app.locals['datefns'] = require('date-fns');
+  res.render('index', {
+    title: 'Mini Message Board',
+    messages,
+  });
+});
+
+/* GET new message page */
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Add a new message' });
+});
+
+/* POST new message */
+router.post('/new', function (req, res, next) {
+  messages.push({
+    user: req.body.name,
+    text: req.body.message,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
